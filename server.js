f8c3dcdce290a1097cc82c0e36d68eb177ba86b8
Matt Star <ms.starakiewicz@gmail.com>
2019-05-09 01:19:20 +0200

    Add app store broker v1.0.0

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store.');
+    next();
+});
+
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Błąd 404. Nie odnaleziono szukanego tematu.')
+});
+
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);   
+});
+
+
+
+
